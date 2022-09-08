391cc377b24af4af56cb788dd545383ab188cea5
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2022-09-08 16:11:46 +0300

    load next level
    added respawn and next level

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class CollisionHandler : MonoBehaviour
 
             case "Finish":
                 Debug.Log("End of the game");
+                NextLevel();
                 break;
 
             case "Fuel":
@@ -22,7 +24,26 @@ public class CollisionHandler : MonoBehaviour
 
             default:
                 Debug.Log("Boom!");
+                ReloadScene();
                 break;
         }
+    }
+
+    private static void NextLevel()
+    {
+        int currentSceneIndex2 = SceneManager.GetActiveScene().buildIndex;
+        int nextLevelIndex = currentSceneIndex2 + 1;
+
+        if (nextLevelIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextLevelIndex = 0;
+        }
+        SceneManager.LoadScene(nextLevelIndex);
+    }
+
+    private static void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
